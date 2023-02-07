96a277b70f55425c4b47e0607a1090c79d676aa3
Sam Black <sam.black@lubbockisd.org>
2023-02-07 08:46:51 -0600

    Day 1 notes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //Arrays
+        // creating an array of integers named dave with 23 total spots
+        // numbered from 0 .. 22
+        int[] dave = new int[23];
+        System.out.println(dave);   // prints out memory address
+        System.out.println(Arrays.toString(dave));  //uses utility class to display everything with []s and ", "s
+        System.out.println(dave[4]);    // only prints out the value in spot #4
+        for(int i = 0; i < dave.length; i++)    //dave.length give the value of how many total spots there are
+            dave[i] = i;
+        System.out.println(Arrays.toString(dave));
+        System.out.println(dave[23]);
+
     }
 }
