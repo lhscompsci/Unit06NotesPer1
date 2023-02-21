01fca1045417701a25add953fdc6eef37e2750a4
Sam Black <sam.black@lubbockisd.org>
2023-02-21 08:25:12 -0600

    Part 2 notes

---
+++ src/Herd.java
@@ -0,0 +1,26 @@
+public class Herd
+{
+    private Creature[] things;
+
+    public Herd()
+    {
+        //must size the array
+
+    }
+
+    public Herd(int size)
+    {
+        //must size the array
+
+    }
+
+    public void set(int spot, int creatureSize )
+    {
+        //set spot to new Creature
+    }
+
+    public String toString()
+    {
+        return "" + Arrays.toString( things );
+    }
+}
